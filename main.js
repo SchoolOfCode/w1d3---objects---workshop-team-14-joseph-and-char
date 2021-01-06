f57aab3b68038516f9cb3a1f8bf57d64760b8222
Joseph Alton <45772406+ParadigmZero@users.noreply.github.com>
2021-01-06 12:09:59 +0000

    Update main.js
    Another object.

--- main.js
+++ main.js
@@ -3,5 +3,15 @@ let myHouse={ stories: 2,
              bedrooms:2,
              garden:1,
              energyEffRating : "D" }
+
+let myCar={  colour: "red",
+           wheels:4,
+            powerSteering: true,
+            seats:2,
+            milesPerGallon: 20
+          }
+
+
+
              
              
